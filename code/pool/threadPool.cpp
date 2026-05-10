fe6063789c3fc1225a1aa4859265b3b123b51b38
deng <908466295@qq.com>
2026-05-11 01:39:27 +0800

    feat:new threadpool

--- code/pool/threadPool.cpp
+++ code/pool/threadPool.cpp
@@ -1,34 +1,44 @@
 #include "threadPool.h"
 
-ThreadPool::ThreadPool(int ThreadCount) : pool_(std::make_shared<Pool>()) {
+ThreadPool::ThreadPool(int ThreadCount) {
     assert(ThreadCount > 0);
-    for (int i = 0; i < ThreadCount; ++i) {
-        std::thread([this] {
-            std::unique_lock<std::mutex> locker(pool_->mtx);
-            while (true) {
-                if (!pool_->tasks.empty()) {
-                    auto task = std::move(pool_->tasks.front());
-                    pool_->tasks.pop();
 
-                    locker.unlock();
-                    task();
-                    locker.lock();
-                }
-                else if (pool_->isClose) {
-                    break;
-                }
-                else {
-                    pool_->cond.wait(locker);
-                }
-            }
-            }).detach();
+    for (int i = 0; i < ThreadCount; ++i) {
+        workers_.emplace_back(&ThreadPool::_workLoop, this);
     }
 }
 
 ThreadPool::~ThreadPool() {
-    std::lock_guard<std::mutex> locker(pool_->mtx);
-    if (pool_) {
-        pool_->isClose = true;
-        pool_->cond.notify_all();
+    {
+        std::lock_guard locker(mtx_);
+        isClose_ = true;
+    }
+    cond_.notify_all();     // 唤醒所有阻塞的线程，处理剩余的任务，并结束
+
+    for (auto&& worker : workers_) {
+        if (worker.joinable()) {
+            worker.join();
+        }
+    }
+}
+
+void ThreadPool::_workLoop() {
+    while (true) {
+        std::unique_lock locker(mtx_);
+        cond_.wait(locker, [this]() {return isClose_ || !tasks_.empty();});     // 线程池关闭，或者任务队列非空
+
+        if (isClose_ && tasks_.empty()) break;   // 线程池关闭了，所有任务执行完毕
+
+        auto task = tasks_.front();
+        tasks_.pop();
+        locker.unlock();
+
+        try {
+            task();
+        }
+        catch (const std::exception& e) {
+
+            std::cerr << e.what() << '\n';
+        }
     }
 }
--- code/pool/threadPool.h
+++ code/pool/threadPool.h
@@ -5,36 +5,52 @@
 #include <queue>
 #include <functional>
 #include <thread>
+#include <future>
+#include <iostream>
 #include <assert.h>
 
 class ThreadPool {
 public:
-    ThreadPool() = default;
+    ThreadPool(const ThreadPool& o) = delete;
+    ThreadPool(ThreadPool&& o) = delete;
+    ThreadPool& operator=(const ThreadPool& o) = delete;
+    ThreadPool& operator=(ThreadPool&& o) = delete;
+
     explicit ThreadPool(int ThreadCount = 8);
     ~ThreadPool();
 
-    template<typename T>
-    void AddTask(T&& Task);
+    template<typename F, typename... Args>
+    auto AddTask(F&& Task, Args&&... args) -> std::future<decltype(Task(args...))>;
 private:
-    struct Pool
-    {
-        bool isClose;
-        std::queue<std::function<void()>> tasks;
+    void _workLoop();
+private:
+    bool isClose_ = false;
+    std::vector<std::thread> workers_;
+    std::queue<std::function<void()>> tasks_;
 
-        std::mutex mtx;
-        std::condition_variable cond;
-    };
-
-    std::shared_ptr<Pool> pool_;
+    std::mutex mtx_;
+    std::condition_variable cond_;
 };
 
-template<typename T>
-inline void ThreadPool::AddTask(T&& Task) {
-    std::unique_lock<std::mutex> locker(pool_->mtx);
-    if (pool_->isClose) {
-        return;
+template<typename F, typename ...Args>
+inline auto ThreadPool::AddTask(F&& Task, Args && ...args) -> std::future<decltype(Task(args ...))> {
+    using ReturnType = decltype(Task(args...)); // 获取task返回值能行
+
+    // 用packaged_task包裹任务函数，用于future获取任务函数的返回值。packaged_task只能移动不能拷贝。
+    // 用shared_ptr是因为task是局部变量，而任务执行是在工作线程中，不用shared_ptr就被释放了。
+    auto task = std::make_shared<std::packaged_task<ReturnType()>>(
+        std::bind(std::forward<F>(Task), std::forward<Args>(args)...)
+    );
+
+    std::future<ReturnType> future = task->get_future();
+    {
+        std::lock_guard<std::mutex> locker(mtx_);
+        if (isClose_) {
+            throw std::runtime_error("ThreadPool has closed!");
+        }
+        tasks_.emplace([task]() {(*task)();});
     }
 
-    pool_->tasks.emplace(std::forward<T>(Task));
-    pool_->cond.notify_one();
+    cond_.notify_one();     // 添加任务后，唤醒一个线程去执行。
+    return future;
 }
